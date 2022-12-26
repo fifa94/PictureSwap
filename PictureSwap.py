1d7c578a14c164239dcc1bf9aaee58aa116d4dae
Filip Sramek <filip.sramek@br-automation.com>
2022-12-26 23:00:25 +0100

    Added method which can create list of all jpg files

--- PictureSwap.py
+++ PictureSwap.py
@@ -1,4 +1,24 @@
+import os
+
 
 class PictureFolder:
+
     def __init__(self, folder_path):
+
+        if not isinstance(folder_path, str):
+            raise TypeError("Input must be string")
+
         self.folder_path = folder_path
+        self.list_of_files = []
+
+    def get_all_files(self):
+
+        self.list_of_files = [file for file in os.listdir(self.folder_path) if file.endswith('.jpg')]
+
+        return self.list_of_files
+
+
+Path = 'C:\\Users\\Public\\Pictures'
+
+folder_object = PictureFolder(Path)
+print(folder_object.get_all_files())
